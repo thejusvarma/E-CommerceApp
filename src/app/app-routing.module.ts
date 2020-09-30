2040eefe014dd699422a73a9989d6e7b776358c3
Priyanshu Belwal <priyanshubelwalpb@gmail.com>
2020-10-01 04:51:52 +0530

    Branches Are merged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {DealOfTheDayComponent} from './deal-of-the-day/deal-of-the-day.component';
+import {MyCartComponent} from './my-cart/my-cart.component';
+import {OrderPlaceSuccessComponent} from './order-place-success/order-place-success.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'daily-deals', component: DealOfTheDayComponent },
+  { path: 'cart', component: MyCartComponent },
+  { path: 'order-placed', component: OrderPlaceSuccessComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+// @ts-ignore
 export class AppRoutingModule { }
