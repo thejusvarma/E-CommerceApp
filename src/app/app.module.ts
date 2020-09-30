eea8dbfc3a1707a6008dd01069f698594c228e71
Priyanshu Belwal <priyanshubelwalpb@gmail.com>
2020-09-30 08:51:30 +0530

    Footer HTML, CSS Working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
