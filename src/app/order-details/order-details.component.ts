84af8649cb1c6e1e5a9dcd8003d853bd6d3d1eb7
Asmita Paul <asmitapaul2000@gmail.com>
2020-10-06 01:41:33 +0530

    opt functionality and dummy images added to similar products & trending products

--- src/app/order-details/order-details.component.ts
+++ src/app/order-details/order-details.component.ts
@@ -6,19 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-details.component.css']
 })
 export class OrderDetailsComponent implements OnInit {
-  dataRes = [{
+  dataRe = [
+    {
+    orderstatus: "shipped",
+    cancellationDate: "Fri, 18th sep",
+    cancellationTime: "4:50 pm",
+    cancellationReason: "Due to high demand and limited stock, your item was cancelled.",
+    deliveredConfirm: "Your Item has been Delivered.",
+    deliveredStatus: "Delivered on Wed, Sep 30",
+    shippedConfirm: "Your Item is out for delivery.",
+    placedConfirm: "Delivery expected by Oct 06.",
+    shippedStatus:"Delivery Expected by Tomorrow, Oct 06",
+    productDetails: {
+      img:"https://dummyimage.com/150x200/00e1ff/000000",
+      productname: "Redmi Note 6 (64GB)",
+      price: 20000,
+      color: "white",
+      seller: "XONIGHT",
+    },
+    orderBar : ["Ordered","Packed","Shipped"] 
+  },
+  {
     orderstatus: "deliver",
     cancellationDate: "Fri, 18th sep",
     cancellationTime: "4:50 pm",
     cancellationReason: "Due to high demand and limited stock, your item was cancelled.",
     deliveredConfirm: "Your Item has been Delivered.",
+    deliveredStatus: "Delivered on Wed, Sep 30",
+    shippedConfirm: "Your Item is out for delivery.",
+    placedConfirm: "Delivery expected by Oct 06.",
+    shippedStatus:"Delivery Expected by Tomorrow, Oct 06",
     productDetails: {
+      img:"https://dummyimage.com/150x200/00e1ff/000000",
       productname: "Redmi Note 6 (64GB)",
       price: 20000,
       color: "white",
       seller: "XONIGHT",
-    }
-  }]
+    },
+    orderBar :["Ordered","Packed","Shipped","Delivered"]
+  }
+]
 
   dataUser = {
     address: "HIG- 5/651 A.P.H.B. Colony, Near Chandini Chowk, Delhi",
@@ -34,7 +61,7 @@ export class OrderDetailsComponent implements OnInit {
 
 public counts = ["Ordered","Packed","Shipped","Delivered"];
 public counts1=["Ordered","Cancelled"]
-  public orderStatus = ["Ordered","Packed"];
+//   public orderStatus = ["Ordered","Packed"];
   
   constructor() { }
 
--- src/app/otp/otp.component.ts
+++ src/app/otp/otp.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-otp',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtpComponent implements OnInit {
 
+@ViewChild('otp1') otp1: ElementRef;
+@ViewChild('otp2') otp2: ElementRef;
+@ViewChild('otp3') otp3: ElementRef;
+@ViewChild('otp4') otp4: ElementRef;
+@ViewChild('otp5') otp5: ElementRef;
+@ViewChild('otp6') otp6: ElementRef;
+
+ str:any='';
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
+  ngOnInit(): void {
+     
+   
+  }
+  
+   display(){
+    this.str= this.otp1.nativeElement.value+this.otp2.nativeElement.value+this.otp3.nativeElement.value+this.otp4.nativeElement.value+this.otp5.nativeElement.value+this.otp6.nativeElement.value;
+    
+    console.log(this.str)
+   }
+   
+  // var name:string;
+  // var str= this.otp1.nativeElement.value
 }
+	
