b2e04e21682eac46e5eb8ffaf8230cc872b5bef4
Lavanya5f2 <66686033+Lavanya-2211@users.noreply.github.com>
2020-10-10 21:57:23 +0530

    commit on lavanya

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -10,22 +10,22 @@ export class OrdersComponent implements OnInit {
 
   orders = [
 
-  { orderid: 5467389201, items: [{img: 'https://dummyimage.com/400/000/fff' ,
+  { orderid: 5467389201, items: [{img: 'https://dummyimage.com/800/000/fff' ,
   name: 'product name', orderstatus: 0, price: 300 , size: 2 , status: 'Not yet dispatched' ,  quantity: 2 , color: 'yellow'},
 
-  {img: 'https://dummyimage.com/400/000/fff' , name: 'product name', orderstatus: 0, price: 300 , size: 4 ,
+  {img: 'https://dummyimage.com/800/000/fff' , name: 'product name', orderstatus: 0, price: 300 , size: 4 ,
   status: ' delivery expected by 5th october 2020' ,  quantity: 3, color: 'yellow'}] } ,
 
-  { orderid: 5467389202, items: [{img: 'https://dummyimage.com/400/000/fff' ,
+  { orderid: 5467389202, items: [{img: 'https://dummyimage.com/800/000/fff' ,
   name: 'product name', orderstatus: 0, price: 500 , size: 1 , status: 'dispatched' ,  quantity: 2,  color: 'red'}] },
 
-  { orderid: 5467389203, items: [{img: 'https://dummyimage.com/400/000/fff' ,
+  { orderid: 5467389203, items: [{img: 'https://dummyimage.com/800/000/fff' ,
   name: 'product name', orderstatus: 1, price: 500 , size: 4 , status: 'delivered',  quantity: 1, color: 'green'},
-  {img: 'https://dummyimage.com/400/000/fff' ,
+  {img: 'https://dummyimage.com/800/000/fff' ,
   name: 'product name', orderstatus: -1, price: 300 , size: 3 , status: 'Cancelled',  quantity: 1, color: 'yellow'}] } ,
 
 
-  { orderid: 5467389204, items: [{img: 'https://dummyimage.com/400/000/fff' ,
+  { orderid: 5467389204, items: [{img: 'https://dummyimage.com/800/000/fff' ,
   name: 'product name', orderstatus: -1, price: 3000 , size: 4 , status: 'item returned' ,  quantity: 1,  color: 'red'}] },
   ];
 
