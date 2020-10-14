69c3cc711512375b4bb96b386c8613d8ffae3dc0
Thejus Varma <thejusvarma11@gmail.com>
2020-10-14 12:29:39 +0530

    imp

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  @Input()
-  autoFocus: false;
+ 
   constructor() { }
 
   ngOnInit(): void {
