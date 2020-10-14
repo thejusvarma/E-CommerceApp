7b3c690f3ed96c00ecdcb665caf2d28d1f4daeb5
Asmita Paul <asmitapaul2000@gmail.com>
2020-10-14 10:03:54 +0530

    changes done

--- src/app/deal-of-the-day/deal-of-the-day.component.ts
+++ src/app/deal-of-the-day/deal-of-the-day.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-deal-of-the-day',
@@ -6,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deal-of-the-day.component.css']
 })
 export class DealOfTheDayComponent implements OnInit {
-
-  constructor() { }
-
+  subscription: Subscription;
+  remainingTime : String = '';
+  constructor() { 
+    this.subscription= interval(1000).subscribe((x =>{
+      this.updateRemainingTime();
+  }));
+  }
+  updateRemainingTime()
+  {
+    var actualTime: Date = new Date(Date.now());
+    var endOfDay : Date = new Date(actualTime.getFullYear(), actualTime.getMonth(), actualTime.getDate() + 1, 0, 0, 0);
+    var diffInSeconds = Math.floor(Math.abs(endOfDay.getTime() - actualTime.getTime()) / 1000);
+  
+    var seconds : any = diffInSeconds % 60;
+  
+    diffInSeconds = diffInSeconds - seconds;
+  
+    var diffInMinutes = diffInSeconds / 60;
+  
+    var minutes : any= diffInMinutes % 60;
+  
+    diffInMinutes = diffInMinutes - minutes;
+  
+    var hours : any= diffInMinutes / 60 ;
+  
+    seconds = seconds.toString();
+    minutes = minutes.toString();
+    hours = hours.toString();
+  
+   if(seconds.length == 1)
+    seconds = '0' + seconds;
+  
+    if(minutes.length == 1)
+    minutes = '0' + minutes;
+  
+    if(hours.length == 1)
+    hours = '0' + hours;
+  
+    var diff = hours + ' : ' + minutes + ' : ' + seconds
+  
+    this.remainingTime = diff;
+  
+  }
   ngOnInit(): void {
   }
 
